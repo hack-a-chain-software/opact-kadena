5093b88fb5b60091d1561389a1032ce081263dbc
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-04-26 13:18:12 -0300

    test

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,50 @@
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import stdLibBrowser from "node-stdlib-browser";
+import inject from "@rollup/plugin-inject";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+/**
+ * https://vitejs.dev/config/
+ */
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      ...inject({
+        global: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "global",
+        ],
+        process: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "process",
+        ],
+        Buffer: [
+          require.resolve("node-stdlib-browser/helpers/esbuild/shim"),
+          "Buffer",
+        ],
+      }),
+      enforce: "post",
+    },
+  ],
+  build: {
+    target: ["es2022"],
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer", "process", "fs"],
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      ...stdLibBrowser,
+      // fs: "./node_modules/browserify-fs/index.js",
+      // fs: "./node_modules/fs-extra/lib/index.js",
+    },
+  },
+});
