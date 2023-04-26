af24d5b7d65d84f15c6f3a1e3a9fe4622afa71ec
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-04-26 13:18:12 -0300

    fix: lefthook

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { verifyProof } from "./generate_calldata";
+
+const x = 0;
+const y = 0;
+
+const verify = async () => {
+  const abi = { x: 0, y: 0 };
+
+  await verifyProof(abi).catch((error) => {
+    console.error(error);
+  });
+};
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +30,8 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+
+    <button @click="verify()">Click here to test verify proof</button>
   </main>
 </template>
 
