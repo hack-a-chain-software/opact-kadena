53b974c0278d166764df329c93ea1a2a3dd6dc48
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-06-06 11:46:18 -0300

    feat(indexer): api final version

---
+++ indexer/api/src/resolvers.ts
@@ -0,0 +1,50 @@
+import { baseRequest } from "./utils";
+
+export const resolvers = {
+  Query: {
+    getEvents: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query = "SELECT * FROM events LIMIT $1 OFFSET $2;";
+
+      const result = await baseRequest(query, [args.size, offset]);
+
+      return result.rows;
+    },
+
+    getEventsByModule: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query =
+        "SELECT * FROM events WHERE module = '$1' LIMIT $2 OFFSET $3;";
+
+      const result = await baseRequest(query, [args.module, args.size, offset]);
+
+      return result.rows;
+    },
+
+    getTransactions: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query = "SELECT * FROM transactions LIMIT $1 OFFSET $2;";
+
+      const result = await baseRequest(query, [args.size, offset]);
+
+      return result.rows;
+    },
+
+    getTransactionsByRequestKey: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query = `SELECT * FROM transactions WHERE requestkey = $1 LIMIT $2 OFFSET $3;`;
+
+      const result = await baseRequest(query, [
+        args.requestkey,
+        args.size,
+        offset,
+      ]);
+
+      return result.rows;
+    },
+  },
+};
