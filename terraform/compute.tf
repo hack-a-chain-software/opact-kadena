cb1664d99a3021f06fd34d4fadc7dd2a3c33e457
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-06-06 11:36:20 -0300

    feat(front): update api

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -3,59 +3,59 @@ resource "aws_eip" "chainweb_node_ip" {
   vpc      = true
 }
 
-# resource "aws_instance" "chainweb_data" {
-#   ami           = "ami-004dac467bb041dc7"
-#   instance_type = "t2.micro"
-#   associate_public_ip_address = true
+resource "aws_instance" "chainweb_data" {
+  ami           = "ami-004dac467bb041dc7"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
 
-#   subnet_id = "${aws_subnet.indexer.id}"
+  subnet_id = "${aws_subnet.indexer.id}"
 
-#   key_name = "${aws_key_pair.opact-ssh.key_name}"
+  key_name = "${aws_key_pair.opact-ssh.key_name}"
 
-#   tags = {
-#     Name = "chainweb-data"
-#   }
+  tags = {
+    Name = "chainweb-data"
+  }
 
-#   vpc_security_group_ids      = ["${aws_security_group.indexer.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.indexer.id}"]
 
-#   connection {
-#     type        = "ssh"
-#     user        = "ubuntu"
-#     private_key = file(var.pvt_key)
-#     host        = self.public_ip
-#   }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.pvt_key)
+    host        = self.public_ip
+  }
 
-#   provisioner "file" {
-#     source      = "../indexer/chainweb-data"
-#     destination = "."
-#   }
+  provisioner "file" {
+    source      = "../indexer/chainweb-data"
+    destination = "."
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo apt-get update",
-#       "sudo apt-get remove docker docker-engine docker.io containerd runc",
-#       "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
-#       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-#       "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-#       "sudo apt-get update",
-#       "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-#       "sudo groupadd docker",
-#       "sudo usermod -aG docker ubuntu",
-#       "sudo systemctl enable docker.service",
-#       "sudo systemctl enable containerd.service",
-#       "sudo apt-get install -y jq",
-#       "cd ./chainweb-data",
-#       "echo \"CWD_DB_HOST=${aws_db_instance.chainweb_database.address}\" | sudo tee .env",
-#       "echo \"CWD_DB_USER=${aws_db_instance.chainweb_database.username}\" | sudo tee -a .env",
-#       "echo \"CWD_DB_PASS=${aws_db_instance.chainweb_database.password}\" | sudo tee -a .env",
-#       "echo \"CWD_DB_PORT=5432\" | sudo tee -a .env",
-#       "echo \"CWD_DB_NAME=indexer\" | sudo tee -a .env",
-#       "echo \"CWD_NODE=146.190.198.163\" | sudo tee -a .env",
-#       "sudo setfacl --modify user:ubuntu:rw /var/run/docker.sock",
-#       "sudo docker compose up chainweb-data -d",
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get remove docker docker-engine docker.io containerd runc",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "sudo groupadd docker",
+      "sudo usermod -aG docker ubuntu",
+      "sudo systemctl enable docker.service",
+      "sudo systemctl enable containerd.service",
+      "sudo apt-get install -y jq",
+      "cd ./chainweb-data",
+      "echo \"CWD_DB_HOST=${aws_db_instance.chainweb_database.address}\" | sudo tee .env",
+      "echo \"CWD_DB_USER=${aws_db_instance.chainweb_database.username}\" | sudo tee -a .env",
+      "echo \"CWD_DB_PASS=${aws_db_instance.chainweb_database.password}\" | sudo tee -a .env",
+      "echo \"CWD_DB_PORT=5432\" | sudo tee -a .env",
+      "echo \"CWD_DB_NAME=indexer\" | sudo tee -a .env",
+      "echo \"CWD_NODE=146.190.198.163\" | sudo tee -a .env",
+      "sudo setfacl --modify user:ubuntu:rw /var/run/docker.sock",
+      "sudo docker compose up chainweb-data -d",
+    ]
+  }
+}
 
 # In summary, this resource defines the creation of an AWS EC2 instance named "chainweb-node"
 # with specific configuration and provisions it with necessary software and configurations to run a Kadena's Chainweb Node.
