1841f82fd65b2f46c962dc76e454f770f7b13f76
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-06-21 09:54:03 -0300

    test(front): scrollbar

--- front/apps/landing/layouts/landing.vue
+++ front/apps/landing/layouts/landing.vue
@@ -1,10 +1,6 @@
 <template>
   <div
-    class="
-      scrollbar
-      scrollbar-thumb-red-600
-      scrollbar-track-red-600
-    "
+    class="scrollbar"
   >
     <header>
       <LandingHeaderNav />
@@ -20,16 +16,5 @@
 <style>
 body {
   background-color: #060a0f;
-}
-
-body::-webkit-scrollbar {
-  -webkit-appearance: none !important;
-  width: 4px !important;
-}
-
-body::-webkit-scrollbar-thumb {
-  border-radius: 4px !important;
-  background-color: rgba(250, 250, 250, 0.2) !important;
-  -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5) !important;
 }
 </style>
