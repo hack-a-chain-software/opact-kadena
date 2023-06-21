980cad40c6fe3d2bc51594682e6f6bd8405a44fd
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-06-21 10:57:11 -0300

    feat(front): new scrollbar

--- front/apps/landing/layouts/landing.vue
+++ front/apps/landing/layouts/landing.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="scrollbar"
-  >
+  <div>
     <header>
       <LandingHeaderNav />
 
@@ -16,5 +14,16 @@
 <style>
 body {
   background-color: #060a0f;
+}
+
+body::-webkit-scrollbar {
+  -webkit-appearance: none !important;
+  width: 8px !important;
+}
+
+body::-webkit-scrollbar-thumb {
+  border-radius: 20px !important;
+  background-color: #202327CC !important;
+  -webkit-box-shadow: 0 0 1px rgba(255,255,255,.5) !important;
 }
 </style>
