42d80682364eb21b2cc78806e4d337e8a5597061
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-07-04 16:19:57 -0300

    test

--- front/apps/site/components/site/figure/Blue.vue
+++ front/apps/site/components/site/figure/Blue.vue
@@ -7,35 +7,8 @@
       h-full
       absolute
       bottom-0
-      pointer-events-none
-      select-none
     "
   >
-    <figure
-      class="
-        opacity-0
-        duration-700
-        transition-all
-        group-hover/container:opacity-100
-      "
-    >
-      <nuxt-img
-        quality="100"
-        src="/wallet/blue-hover.webp"
-        class="absolute inset-0 w-full h-full z-[-1]"
-      />
-    </figure>
-
-    <figure class="h-[700px] w-full absolute bottom-0">
-      <nuxt-img
-        quality="100"
-        src="/wallet/blue.webp"
-        loading="lazy"
-        class="w-full h-full"
-        alt="Illustrative image"
-        sizes="xxl:704px"
-      />
-    </figure>
 
     <figure
       class="
@@ -46,14 +19,17 @@
         md:translate-x-[100px] md:bottom-[-100px]
         lg:translate-x-[46px] lg:bottom-[-40px]
         xl:translate-x-[68px] xl:bottom-[-80px]
-        2xl:translate-x-[40px] 2xl:bottom-[-125px]
+        2xl:translate-x-[0px] 2xl:bottom-[0px]
+
+
       "
     >
-      <nuxt-img
+      <NuxtImg
         quality="100"
         alt="Illustrative image"
-        src="/wallet/figures/bring.webp"
-        sizes="639:290px 1023:500px 1279:350px 1439:440px 2560:982px"
+        src="/wallet/figures/bring3x.webp"
+        sizes="lg:800px 2560:1472px"
+        class="nuxt-img-blue"
       />
     </figure>
   </div>
--- front/apps/site/components/site/figure/Purple.vue
+++ front/apps/site/components/site/figure/Purple.vue
@@ -8,35 +8,8 @@
       absolute
       bottom-0
       left-0
-      pointer-events-none
-      select-none
     "
   >
-    <figure
-      class="
-        opacity-0
-        group-hover/container:opacity-100
-        transition-all
-        duration-700
-      "
-    >
-      <nuxt-img
-        quality="100"
-        src="/wallet/purple-hover.webp"
-        class="absolute h-full w-full z-[-1]"
-      />
-    </figure>
-
-    <figure class="h-[700px] w-full absolute bottom-0">
-      <nuxt-img
-        quality="100"
-        src="/wallet/purple.webp"
-        loading="lazy"
-        class="w-full h-full"
-        alt="Illustrative image"
-        sizes="xs:328px sm:704px"
-      />
-    </figure>
 
     <figure
       class="
@@ -47,15 +20,12 @@
         md:translate-x-[110px] md:bottom-[-96px]
         lg:translate-x-[48px] lg:bottom-[-63px]
         xl:translate-x-[70px] xl:bottom-[-65px]
-        2xl:translate-x-[85px] 2xl:bottom-[-94px]
+        2xl:translate-x-[5px] 2xl:bottom-[0px]
       "
     >
-      <nuxt-img
-        quality="100"
-        sizes="639:302px 1023:500px 1279:350px 1439:430px 2560:468px"
-        src="/wallet/figures/integrate.webp"
+      <img
         alt="Illustrative image"
-        class="2xl:w-[468px]"
+        src="/wallet/figures/bring3x.webp"
       />
     </figure>
   </div>
