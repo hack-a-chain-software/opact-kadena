faa173c86bb90ddb8c1f67ce6eb84411394ef2f8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-07-04 18:12:53 -0300

    f

--- front/apps/site/components/site/wallet/About.vue
+++ front/apps/site/components/site/wallet/About.vue
@@ -17,56 +17,6 @@
       <SiteFigureBlue />
     </template>
 
-    <template #title>
-      <span
-        class="
-          block
-          mb-4
-          sm:mb-6
-          lg:mb-4
-          xl:w-[493px]
-          text-center
-          pt-[6px]
-          sm:pt-0
-          z-[3]
-          relative
-          lg:text-[32px] lg:font-semibold lg:leading-normal
-          2xl:text-[34px] 2xl:pt-0
-          xl:pt-1
-          mx-auto
-        "
-      >
-        Bring private and compliant accounts to your
-        blockchain
-      </span>
-    </template>
-
-    <template #text>
-      <span
-        class="
-          block
-          mb-6
-          lg:max-w-[340px]
-          xl:max-w-full
-          lg:mb-[21px]
-          2xl:mb-[28px]
-          text-center
-          z-[8]
-          relative
-          lg:text-lg lg:font-medium lg:leading-normal
-          mx-auto
-          2xl:w-[532px]
-        "
-      >
-        Opact Wallet can be implemented into different
-        blockchain ecosystems, and our team has the
-        expertise to develop it in Solidity, Rust, Ink!,
-        Pact, and other languages and frameworks. If you
-        want private transactions on your blockchain, reach
-        out to us!
-      </span>
-    </template>
-
     <template #cta>
       <Button
         disabled
