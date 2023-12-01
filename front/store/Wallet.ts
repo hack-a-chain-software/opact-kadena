d4d5fcc476310699a3f891863c404ea817be3008
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-01 11:10:38 -0300

    feat: create wallet store

---
+++ front/store/Wallet.ts
@@ -0,0 +1,70 @@
+import { defineStore } from 'pinia'
+import { getWalletFromMnemonic } from 'opact-sdk'
+import { shortenAddress } from '~/utils/string'
+import { useAuthStorage } from '~/hooks/auth-storage'
+import { useStateStorage } from '~/hooks/state-starage'
+
+export const useWalletStore = defineStore({
+  id: 'opact-wallet',
+
+  state: (): any => {
+    const { cache } = useAuthStorage()
+
+    return {
+      cache,
+      account: null
+    }
+  },
+
+  getters: {
+    connected: (state: any): boolean => {
+      const { account } = state
+
+      return !!account
+    },
+    truncatedAddress: (state: any): string =>
+      shortenAddress(state.node.address)
+  },
+  actions: {
+    connect (mnemonic: string) {
+      if (!mnemonic) {
+        throw new Error('Needs mnemonic to connect')
+      }
+
+      const newAccount = getWalletFromMnemonic(mnemonic)
+
+      this.persistAuth(mnemonic)
+
+      this.value = newAccount
+
+      return newAccount
+    },
+
+    persistAuth (mnemonic: any) {
+      const { store } = useAuthStorage()
+
+      const newCache = {
+        phrase: mnemonic
+      }
+
+      store(newCache)
+    },
+
+    logout () {
+      const { clear } = useAuthStorage()
+      const { clear: clearState } = useStateStorage()
+
+      const router = useRouter()
+
+      clear()
+      clearState()
+
+      this.cache = null
+      this.account = null
+
+      router.push({
+        path: '/auth'
+      })
+    }
+  }
+})
