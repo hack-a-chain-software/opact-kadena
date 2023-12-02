4f7844f3304b89a71a8b40ab69e0426a99561ae8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-02 17:16:18 -0300

    Update guest.ts

--- front/middleware/guest.ts
+++ front/middleware/guest.ts
@@ -4,13 +4,10 @@ export default defineNuxtRouteMiddleware(() => {
   const wallet = useWalletStore()
   const router = useRouter()
 
-  console.log('wallet.connected', wallet?.connect)
-  console.log('wallet.cache.phrase', wallet?.cache?.phrase)
+  console.log('wallet.connected', wallet?.connected)
+  console.log('wallet.cache.phrase', wallet?.cache)
 
-  if (
-    wallet.connected ||
-    (wallet.cache && wallet.cache.phrase)
-  ) {
+  if ((wallet.cache && wallet.cache.phrase)) {
     return router.push({
       path: '/home'
     })
