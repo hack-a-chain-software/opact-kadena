23a17611264a56c28bac7a0954406be9f00cd8c4
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-08 16:26:36 -0300

    fix(api): resolvers bug

--- indexer/api/src/resolvers.ts
+++ indexer/api/src/resolvers.ts
@@ -2,6 +2,115 @@ import { baseRequest } from "./utils";
 
 export const resolvers = {
   Query: {
+    getCommitments: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query =
+        "SELECT * FROM events WHERE name = new-commitment AND module = $1 AND chainid = $2 LIMIT $3 OFFSET $4";
+
+      const result = await baseRequest(query, [
+        args.module,
+        args.chainId,
+        args.size,
+        offset,
+      ]);
+
+      const events =
+        result?.rows.map(({ params }) => {
+          const value = params[0]?.int;
+
+          const order = params[1]?.int;
+
+          return {
+            order,
+            value,
+          };
+        }) || [];
+
+      const hasNextPage = events.length === args.size;
+
+      return {
+        events,
+        hasNextPage,
+        currentPage: args.page,
+        itemCount: events.length,
+      };
+    },
+
+    getNullifiers: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query =
+        "SELECT * FROM events WHERE name = new-nullifier AND module = $1 AND chainid = $2 LIMIT $3 OFFSET $4";
+
+      const result = await baseRequest(query, [
+        args.module,
+        args.chainId,
+        args.size,
+        offset,
+      ]);
+
+      const events = result?.rows.map(({ params }) => params[0]?.int) || [];
+
+      const hasNextPage = events.length === args.size;
+
+      return {
+        events,
+        hasNextPage,
+        currentPage: args.page,
+        itemCount: events.length,
+      };
+    },
+
+    getReceipts: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query =
+        "SELECT * FROM events WHERE name = new-transaction AND module = $1 AND chainid = $2 LIMIT $3 OFFSET $4";
+
+      const result = await baseRequest(query, [
+        args.module,
+        args.chainId,
+        args.size,
+        offset,
+      ]);
+
+      const events = result?.rows.map(({ params }) => params[0]?.int) || [];
+
+      const hasNextPage = events.length === args.size;
+
+      return {
+        events,
+        hasNextPage,
+        currentPage: args.page,
+        itemCount: events.length,
+      };
+    },
+
+    getUtxos: async (_, args) => {
+      const offset = (args.page - 1) * args.size;
+
+      const query = "SELECT * FROM events WHERE name = new-encrypted-output AND module = $1 AND chainid = $2 LIMIT $3 OFFSET $4";
+
+      const result = await baseRequest(query, [
+        args.module,
+        args.chainId,
+        args.size,
+        offset,
+      ]);
+
+      const events = result?.rows.map(({ params }) => params[0]?.int) || [];
+
+      const hasNextPage = events.length === args.size;
+
+      return {
+        events,
+        hasNextPage,
+        currentPage: args.page,
+        itemCount: events.length,
+      };
+    },
+
     getEvents: async (_, args) => {
       let query = "SELECT * FROM events";
 
@@ -39,151 +148,20 @@ export const resolvers = {
       }
 
       if (conditions.length > 0) {
-        query += ` WHERE ${conditions.join(' AND ')}`;
+        query += ` WHERE ${conditions.join(" AND ")}`;
       }
 
       const offset = (args.page - 1) * args.size;
 
       queryParams.push(args.size, offset);
 
-      query += ` LIMIT $${queryParams.length - 1} OFFSET $${queryParams.length}`;
+      query += ` LIMIT $${queryParams.length - 1} OFFSET $${
+        queryParams.length
+      }`;
 
       const result = await baseRequest(query, queryParams);
 
-      const events = result.rows;
-
-      const hasNextPage = events.length === args.size;
-
-      return {
-        events,
-        hasNextPage,
-        currentPage: args.page,
-        itemCount: events.length,
-      };
-    },
-
-    getCommitments: async (_, args) => {
-      const eventName = "new-commitment";
-
-      const offset = (args.page - 1) * args.size;
-
-      const query =
-        "SELECT * FROM events WHERE name = $1 AND module = $2 AND chainid = $3 LIMIT $4 OFFSET $5;";
-
-      const result = await baseRequest(query, [
-        eventName,
-        args.module,
-        args.chainId,
-        args.size,
-        offset,
-      ]);
-
-      const events = result.rows.map(({ params }) => {
-        const value = params[0]?.int;
-
-        const order = params[1]?.int;
-
-        return {
-          order,
-          value,
-        };
-      });
-
-      const hasNextPage = events.length === args.size;
-
-      return {
-        events,
-        hasNextPage,
-        currentPage: args.page,
-        itemCount: events.length,
-      };
-    },
-
-    getNullifiers: async (_, args) => {
-      const eventName = "new-nullifier";
-
-      const offset = (args.page - 1) * args.size;
-
-      const query =
-        "SELECT * FROM events WHERE name = $1 AND module = $2 AND chainid = $3 LIMIT $4 OFFSET $5;";
-
-      const result = await baseRequest(query, [
-        eventName,
-        args.module,
-        args.chainId,
-        args.size,
-        offset,
-      ]);
-
-      const events = result.rows.map(({ params }) => {
-        const nullifier = params[0]?.int;
-
-        return nullifier;
-      });
-
-      const hasNextPage = events.length === args.size;
-
-      return {
-        events,
-        hasNextPage,
-        currentPage: args.page,
-        itemCount: events.length,
-      };
-    },
-
-    getReceipts: async (_, args) => {
-      const eventName = "new-transaction";
-
-      const offset = (args.page - 1) * args.size;
-
-      const query =
-        "SELECT * FROM events WHERE name = $1 AND module = $2 AND chainid = $3 LIMIT $4 OFFSET $5;";
-
-      const result = await baseRequest(query, [
-        eventName,
-        args.module,
-        args.chainId,
-        args.size,
-        offset,
-      ]);
-
-      const events = result.rows.map(({ params }) => {
-        const receipt = params[0]?.int;
-
-        return receipt;
-      });
-
-      const hasNextPage = events.length === args.size;
-
-      return {
-        events,
-        hasNextPage,
-        currentPage: args.page,
-        itemCount: events.length,
-      };
-    },
-
-    getUtxos: async (_, args) => {
-      const eventName = "new-encrypted-output";
-
-      const offset = (args.page - 1) * args.size;
-
-      const query =
-        "SELECT * FROM events WHERE name = $1 AND module = $2 AND chainid = $3 LIMIT $4 OFFSET $5;";
-
-      const result = await baseRequest(query, [
-        eventName,
-        args.module,
-        args.chainId,
-        args.size,
-        offset,
-      ]);
-
-      const events = result.rows.map(({ params }) => {
-        const utxo = params[0]?.int;
-
-        return utxo;
-      });
+      const events = result.rows || [];
 
       const hasNextPage = events.length === args.size;
 
