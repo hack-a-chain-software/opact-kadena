95f41ebd7031fcdf2ccd7f3db16fbac919329a8e
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-19 14:34:19 -0300

    feat: new serverless

--- indexer/api/src/utils.ts
+++ indexer/api/src/utils.ts
@@ -24,7 +24,6 @@ export const typeDefs = `#graphql
   }
 
   type Query {
-    getEvents(page: Int!, size: Int!, module: String, chainId: Int, name: String, block: String, requestkey: String): PaginatedEvents
     getUtxos(page: Int!, size: Int!, module: String!, chainId: Int!): PaginatedEvents
     getReceipts(page: Int!, size: Int!, module: String!, chainId: Int!): PaginatedEvents
     getNullifiers(page: Int!, size: Int!, module: String!, chainId: Int!): PaginatedEvents
