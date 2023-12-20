f64700c3b8396d330a2b9218a57e511a78721a88
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-20 16:51:56 -0300

    feat(receive): new flow

---
+++ front/components/Receive/Token/steps/Receive.vue
@@ -0,0 +1,156 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { getTokenDetails } from 'opact-sdk'
+import { useReceiveStore } from '~/stores/receive'
+import { useWalletStore } from '~/stores/wallet'
+
+const receiveStore = useReceiveStore()
+
+const {
+  link,
+  amount,
+  balance,
+  haveFunds,
+  isPrivate,
+  isDisabled,
+  receiveType,
+  selectedToken
+} = storeToRefs(receiveStore)
+
+const wallet = useWalletStore()
+
+const { account } = storeToRefs(wallet)
+
+const emit = defineEmits(['changeStep'])
+
+const { provider } = useExtensions()
+
+const checkFunds = async (token: any) => {
+  await nextTick()
+
+  if (!token || !provider.value) {
+    return
+  }
+
+  try {
+    const details = await getTokenDetails(
+      provider.value.account?.address,
+      token
+    )
+
+    return details.balance
+  } catch (e) {
+    console.warn(e)
+  }
+}
+
+const connected = async () => {
+  const funds = await checkFunds(selectedToken.value)
+
+  balance.value = funds
+
+  if (funds < amount.value) {
+    return
+  }
+
+  emit('changeStep', 'review')
+}
+</script>
+
+<template>
+  <div
+    class="flex flex-col space-y-4 w-full items-center justify-center"
+  >
+    <UICardBody
+      gap="space-y-6"
+    >
+      <UICardHeader
+        title="Enter receiving data"
+        description="You can deposit funds from a Regular Kadena Wallet or receive from another Opact Wallet."
+      />
+
+      <ReceiveType
+        :selected="receiveType"
+        @selected="receiveType = $event"
+      />
+
+      <template
+        v-if="isPrivate"
+      >
+        <UIInputCopy
+          bg="bg-gray-800"
+          :value="account?.address"
+          placeholder="Copy and share your public Opact Wallet address"
+        />
+
+        <div>
+          <span
+            class="text-[20px] text-font-1"
+          >
+            Or generate a payment link
+          </span>
+        </div>
+      </template>
+
+      <UIInputMoney
+        v-model="amount"
+        :disabled="false"
+        :token="selectedToken"
+        label="Amount to Receive"
+        @update:model-value="balance = null"
+      />
+
+      <UIWarning
+        type="error"
+        v-show="haveFunds"
+        v-motion-slide-visible-top
+        desc="You do not have sufficient funds to make this deposit"
+      />
+
+      <SelectToken
+        :token="selectedToken"
+        @selected="selectedToken = $event"
+      />
+
+      <template v-if="isPrivate">
+        <ReceiveFromLink
+          :link="link"
+          :is-disabled="isDisabled"
+          @done="receiveStore.reset()"
+        />
+      </template>
+    </UICardBody>
+
+    <UICardBody
+      v-if="!isPrivate && !isDisabled"
+      gap="space-y-6"
+      bg="md:bg-gray-800"
+    >
+      <UIInputCopy
+        label="Copy or share the custon payment link"
+        :value="link"
+        bg="bg-[#21262D]"
+        border="border-blue-400"
+        tooltip-text="You can share the payment link with other people so they will be able to deposit into your Opact Wallet."
+      />
+
+      <div
+        class="flex flex-col gap-2"
+      >
+        <div>
+          <span
+            class="text-xs text-font-1"
+          >
+            OR
+          </span>
+        </div>
+
+        <SelectWallet
+          label="Connect wallet to deposit"
+          :is-disabled="isDisabled || haveFunds"
+          @connected="connected"
+        />
+      </div>
+    </UICardBody>
+  </div>
+</template>
---
+++ front/components/Receive/Token/steps/Review.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useWalletStore } from '~/stores/wallet'
+import { useReceiveStore } from '~/stores/receive'
+
+const receiveStore = useReceiveStore()
+
+const {
+  amount,
+  progress,
+  isLoading,
+  selectedToken
+} = storeToRefs(receiveStore)
+
+const wallet = useWalletStore()
+const { account } = storeToRefs(wallet)
+
+const { provider } = useExtensions()
+</script>
+
+<template>
+  <UICardBody>
+    <UICardHeader
+      title="Confirm deposit information"
+    />
+
+    <UIInputMoney
+      readonly
+      label="Amount"
+      :model-value="amount"
+    />
+
+    <ProviderUser
+      v-if="provider"
+      label="Your Wallet"
+      :provider="provider"
+    />
+
+    <SelectedToken :token="selectedToken" />
+
+    <TxWrapper
+      :amount="amount"
+      :token="selectedToken"
+      :receiver="account.address"
+      :sender="
+        provider?.account?.address ||
+          provider?.account?.account?.account
+      "
+    />
+
+    <UIButtonInline
+      :loading="isLoading"
+      :label="isLoading ? progress : 'Deposit Now'"
+      @click.prevent="receiveStore.sendDeposit(account)"
+    />
+  </UICardBody>
+</template>
