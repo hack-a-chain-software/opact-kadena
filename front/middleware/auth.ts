3a964cc74267ec1a28824ed6a7530cb94a10ad7e
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-01-18 14:58:46 -0300

    feat(front): update env config

--- front/middleware/auth.ts
+++ front/middleware/auth.ts
@@ -9,7 +9,7 @@ export default defineNuxtRouteMiddleware((to) => {
   if (wallet.cache && wallet.cache.phrase) {
     if (
       to.path.includes('nft') &&
-      appConfig.public.nftDisabled
+      appConfig.public.NFT_DISABLED
     ) {
       return router.push({
         path: '/'
@@ -18,7 +18,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
     if (
       to.path.includes('faucet') &&
-      appConfig.public.faucetDisabled
+      appConfig.public.FAUCET_DISABLED
     ) {
       return router.push({
         path: '/'
--- front/plugins/gtag.client.ts
+++ front/plugins/gtag.client.ts
@@ -3,14 +3,14 @@ import { createGtm } from '@gtm-support/vue-gtm'
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
-  if (!config.public.gtmEnabled) {
+  if (!config.public.GTM_ENABLED) {
     return
   }
 
   nuxtApp.vueApp.use(
     createGtm({
-      id: config.public.gaId,
-      enabled: config.public.gtmEnabled,
+      id: config.public.GA_ID,
+      enabled: config.public.GTM_ENABLED,
       defer: true,
       compatibility: true
     }),
